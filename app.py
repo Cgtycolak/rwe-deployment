01a84d2b3f45f92f2ba7b5d1162649304c0d4191
CagatayColak <cgtycolak@gmail.com>
2024-12-18 11:33:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,5 +11,5 @@ import os
 #production
 if __name__ == '__main__':
     app.jinja_env.auto_reload = True
-    port = int(os.environ.get('PORT', 10000))
+    port = int(os.environ.get('PORT', 8000))
     app.run(host='0.0.0.0', port=port)
