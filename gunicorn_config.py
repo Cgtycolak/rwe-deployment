ba9cc8effd523f7cd3b1603eff56d03c13ee0240
CagatayColak <cgtycolak@gmail.com>
2025-03-20 16:52:49 +0300

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -3,8 +3,10 @@ import os
 # Let Render assign the port
 port = os.getenv('PORT', '8000')
 bind = f"0.0.0.0:{port}"
-workers = 4
+workers = 2
 threads = 2
 timeout = 120
-worker_class = "sync"
+worker_class = "gthread"
 wsgi_app = "wsgi:app" 
+max_requests = 1000
+max_requests_jitter = 200
