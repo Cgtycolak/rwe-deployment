4c391ee95c9f7d616de6d5fec19581852d8113e4
CagatayColak <cgtycolak@gmail.com>
2025-03-20 16:51:08 +0300

    Revert "Fix memory issue"
    This reverts commit 93434faaee3421ab4a1722ede6e37cd63829475a.

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -3,10 +3,8 @@ import os
 # Let Render assign the port
 port = os.getenv('PORT', '8000')
 bind = f"0.0.0.0:{port}"
-workers = 2
+workers = 4
 threads = 2
 timeout = 120
-worker_class = "gthread"
-wsgi_app = "wsgi:app"
-max_requests = 1000
-max_requests_jitter = 200
+worker_class = "sync"
+wsgi_app = "wsgi:app" 
